14c13aa5c2a9f2ead8747f4536677c9a2c08a5b6
WinstonLesuT <138252771+WinstonLesuT@users.noreply.github.com>
2023-11-15 03:11:49 +0000

    listToDo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
 	const [ todo, setTodo ] = useState('');
 	const [ todosList, setTodosList] = useState([]);
@@ -11,26 +10,44 @@ const Home = () => {
 		setTodosList(todosList.concat([todo]));
 		if(todo === "") alert("The input cannot be empty");
 	  };
+
 	return (
 		<div className="container ">
 			<h1 className="text-center rounded my-2 p-3 mb-2 bg-info text-white">
 				To Do List
 			</h1>
-
-			<input type="text" onChange={handleInput} value={todo} />
-			<button type="button" className="btn btn-primary" onClick={handleClick}>Add</button>
+			<div className="d-flex align-items-center">
+				<input type="text" onChange={handleInput} value={todo} />
+				<button 
+					type="button" 
+					className="btn btn-primary ms-2" 
+					onClick={handleClick}
+				>
+					Add
+				</button>
+			</div>
 			
 			<div className="container">
 				<ul className="list-group">
 				{ todosList.map (( data , index) => (
 					<li className="list-group-item" key={index}>
 						<span> {data} </span>
-					<button type="button" className="btn btn-danger">Delete</button>
+					<button 
+						type="button" 
+						className="btn btn-danger"
+						onClick={ () => setTodosList(
+							todosList.filter(
+								(currentIndex) =>  {index !== currentIndex})
+							)
+						}
+					>
+						Delete
+					</button>
 				</li>
 				))}
 				</ul>
 				<div>
-					<p> {todosList.length} pending task</p>
+					<p> {todosList.length} pending {todosList.length === 1 ? "task" : "tasks"}</p>
 				</div>
 			</div>
 		</div>
